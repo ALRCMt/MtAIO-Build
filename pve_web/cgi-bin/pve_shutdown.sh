1eabe3b3b8d4b7cf89c3e9242857edba086d7dc8
ALRCMt <1423104595@qq.com>
2026-04-05 16:27:31 +0800

    Update pve_shutdown.sh

--- pve_web/cgi-bin/pve_shutdown.sh
+++ pve_web/cgi-bin/pve_shutdown.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+echo "关机指令已发送"
+
+#!/bin/ash
+echo "Content-Type: text/plain"
+echo ""
+
 PVE_HOST="x.x.x.x"
 
-echo "正在向 PVE 发送关机指令..."
-
-curl -k -s -X POST \
+if curl -k -s --connect-timeout 2 --max-time 3 -X POST \
   -H "Authorization: PVEAPIToken={PVE_TOKEN_ID}={PVE_TOKEN_SECRET}" \
   -d "command=shutdown" \
-  "https://${PVE_HOST}:8006/api2/json/nodes/pve/status"
-
-echo "关机指令已发送"
+  "https://${PVE_HOST}:8006/api2/json/nodes/pve/status" > /dev/null 2>&1; then
+    echo "OKK"
+fi
